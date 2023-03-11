fbb135972d3b9f91ab67b46c0512a41f8bbc73a1
LucianaPeisina <lucianapeisina@hotmail.com>
2023-03-11 12:11:25 -0300

    Ejercicio 1: Ingresar un valor y verificar si es mayor o menor a 10. Mostrar el mensaje correspondiente por la pantalla.

---
+++ Ejercicio1Varia.py
@@ -0,0 +1,7 @@
+num= input ("Por favor ingrese un número ")
+
+if (int(num) >10):
+    print ( num, " Es mayor que 10")
+if (int(num) <10):
+    print(num, "es menor que 10")
+print("Gracias por usar la aplicación")
