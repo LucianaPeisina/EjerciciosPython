8553266b0940f9d84aeec9eb10d221c32da74560
LucianaPeisina <lucianapeisina@hotmail.com>
2023-03-11 11:53:54 -0300

    Diseñar un programa que Ingrese tres números por teclado. Calcular el promedio. Mostrar la información  por pantalla.

---
+++ Ejercicio5Varia.py
@@ -0,0 +1,6 @@
+num1= input ("Por favor ingrese un número ")
+num2= input ("Ingrese otro número por favor ")
+num3= input ("Ingrese un último número ")
+prom = (int(num1)+int(num2)+int(num3))/3
+
+print("El promedio es..." ,prom)
