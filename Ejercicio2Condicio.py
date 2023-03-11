65642f516d605d20724fd44d853e10cdbb649b71
LucianaPeisina <lucianapeisina@hotmail.com>
2023-03-11 12:20:03 -0300

    Ejercicio 2 Condicionales: Ingresar un valor y verificar si es mayor o menor a 10. Mostrar el mensaje correspondiente por la pantalla.

---
+++ Ejercicio2Condicio.py
@@ -0,0 +1,11 @@
+num1= input ("Por favor ingrese un número ")
+num2= input ("Ingrese otro número por favor ")
+multi = int (num1) * int(num2)
+
+if (int(multi) >100):
+    print ( multi, " Es mayor que 100")
+if (int(multi) == 100):
+    print(multi, "es igual que 100")
+if (int(multi) <100):
+    print(multi, "es menor que 100")
+print("Gracias por usar la aplicación")
