21f10ed4aaacd9397cc4afc745c5792a7d045c90
LucianaPeisina <lucianapeisina@hotmail.com>
2023-03-11 11:24:50 -0300

    Ejercicio 2 Variables

---
+++ Ejercicio2Varia.py
@@ -0,0 +1,8 @@
+num1= input ("Por favor ingrese un número ")
+num2= input ("Ingrese otro número por favor ")
+suma = int (num1) + int (num2) 
+multi = int (num1) * int(num2)
+resta = int(num1) - int(num2)
+divi = int(num1) / int(num2)
+
+print("La suma da" ,suma, "\n La resta da" ,resta, "\n La multiplicación da" ,multi, "\n La división da" ,divi, )
